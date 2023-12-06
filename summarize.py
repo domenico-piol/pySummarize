8113ab0da60954580aa1e55f46b9545141dcffd1
Domenico Piol <domenico@piol.name>
2023-12-06 09:04:05 +0100

    adding flask

--- summarize.py
+++ summarize.py
@@ -1,12 +1,37 @@
-#from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 from transformers import pipeline
+import logging
+import json
+from flask import Flask, request, jsonify
 
-#model_name = "fb-bart-large-cnn"
-#tokenizer = AutoTokenizer.from_pretrained(model_name)
-#model = AutoModelForSequenceClassification.from_pretrained(model_name)
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
-summarizer = pipeline("summarization", model="fb-bart-large-cnn")
+@app.route('/')
+def show_complaints_page():    
+    app.logger.info('Summarizer application root called')
+    htmlPageHeader = "<!DOCTYPE html><html><body>"
+    htmlPageContent = "<h1>Welcome to our complaints summarizer</h1>"
+    htmlPageFooter = "</body></html>"
 
-ARTICLE = """In the evolving landscape of hybrid cloud infrastructure, organizations are confronting the complexities of deploying and managing applications across various regions and zones due to latency, availability, compliance, sovereignty, and security concerns. As demand for multitenancy and specialized workload management grows, the limitations of single-cluster setups become evident, pushing businesses towards a multicluster approach. As organizations navigate this shift, they encounter the daunting task of orchestrating and administering multiple clusters efficiently. The need for a cost-effective, scalable, and holistic multicluster Red Hat OpenShift architecture arises as a business priority to maintain a competitive edge and build innovative applications. Recognizing this, a year ago, we introduced a technology preview of hosted control planes for OpenShift, tailored for efficient multicluster deployments. Hosted control planes, which architecturally decouples the control plane from workloads, presents a scalable and economical solution for multicluster management. It helps streamline cluster provisioning and improves resource allocation by leveraging existing OpenShift management infrastructure and tooling. After a year of valuable feedback from our customers and partners, we are happy to announce that hosted control planes for Red Hat OpenShift is now generally available on two critical on-premises platforms: Baremetal via the agent provider and the Red Hat OpenShift Virtualization provider, marking our first milestone in a long and exciting journey."""
+    return htmlPageHeader + htmlPageContent + htmlPageFooter, 200
 
-print(summarizer(ARTICLE, max_length=130, min_length=30, do_sample=False))
+
+@app.route('/summarize/', methods=['GET'])
+@app.route('/summarize/<article>', methods=['GET'])
+def get_summarization(article=""):   
+    app.logger.info('Summarizing an article') 
+    summarizer = pipeline("summarization", model="fb-bart-large-cnn")
+
+    DUMMY = """In the evolving landscape of hybrid cloud infrastructure, organizations are confronting the complexities of deploying and managing applications across various regions and zones due to latency, availability, compliance, sovereignty, and security concerns. As demand for multitenancy and specialized workload management grows, the limitations of single-cluster setups become evident, pushing businesses towards a multicluster approach. As organizations navigate this shift, they encounter the daunting task of orchestrating and administering multiple clusters efficiently. The need for a cost-effective, scalable, and holistic multicluster Red Hat OpenShift architecture arises as a business priority to maintain a competitive edge and build innovative applications. Recognizing this, a year ago, we introduced a technology preview of hosted control planes for OpenShift, tailored for efficient multicluster deployments. Hosted control planes, which architecturally decouples the control plane from workloads, presents a scalable and economical solution for multicluster management. It helps streamline cluster provisioning and improves resource allocation by leveraging existing OpenShift management infrastructure and tooling. After a year of valuable feedback from our customers and partners, we are happy to announce that hosted control planes for Red Hat OpenShift is now generally available on two critical on-premises platforms: Baremetal via the agent provider and the Red Hat OpenShift Virtualization provider, marking our first milestone in a long and exciting journey."""
+
+    if article == "":
+        article = DUMMY
+
+    responseJson = summarizer(article, max_length=130, min_length=30, do_sample=False)
+    
+    if len(responseJson) > 0:
+        return jsonify(responseJson[0]), 200
+    else:
+        app.logger.error('an error occurred')
+        return "No summarization was possible", 500
+        
